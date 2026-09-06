59d63b2f8aede2996b0613e7df98b6b71cd13f3e
Colin Shevlin <cwshevlin@gmail.com>
2026-09-06 11:31:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# base blog
+
+## To add a page
+
+0. Create a [venv](https://docs.python.org/3/library/venv.html) and pip install
+1. Create a file in content/
+2. Copy the metadata fields from one of the existing files in content/
+3. Write your thing
+4. `make html`
+5. `make serve` and navigate to localhost to view your thing
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+AUTHOR = 'Example Author'
+SITENAME = 'example.com'
+SITEURL = "example.com"
+
+PATH = "content"
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+DEFAULT_PAGINATION = 3
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+ALL_FEED_RSS = 'feeds/all.rss.xml'
+THEME = './themes/example'
+STYLESHEET_URL = '/theme/css/main.css'
+
+EXTRA_PATH_METADATA = {
+    'images/favicon.ico': {'path': 'favicon.ico'},
+}
+
+# Blogroll
+LINKS = (
+)
+
+# Social widget
+SOCIAL = (
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
